17d6aa5605270a0d557e6845331d718cbfabc720
Dabhi akshay <akshaydabhi980@gmail.com>
2026-03-09 17:58:38 +0530

    Delete reversh.py

--- reversh.py
+++
@@ -1,9 +0,0 @@
-start = 1
-end = 20
-
-for i in range(start,end + 1):
-    if i > 1:
-        for j in range(int(i//2)):
-            if j % 2 == i :
-                print (i)
-                break
